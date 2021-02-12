52ea38162ba9a6217e9c6cb0168fe650e816d502
Cailly Florian <florian.cailly@senioradom.com>
2021-02-12 15:38:08 +0100

    Add Calculator

---
+++ src/main/java/com/krgcorporate/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.krgcorporate.calculator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Calculator {
+
+    private double result;
+
+}
--- src/test/java/com/krgcorporate/calculator/CalculatorStepDefinitions.java
+++ src/test/java/com/krgcorporate/calculator/CalculatorStepDefinitions.java
@@ -1,5 +1,26 @@
 package com.krgcorporate.calculator;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
 public class CalculatorStepDefinitions {
+
+    private Calculator calculator;
+
+    @Given("I have a Calculator")
+    public void i_have_a_calculator() {
+        this.calculator = new Calculator();
+    }
+
+    @Given("The current result is {double}")
+    public void theCurrentResultIs(double value) {
+        this.calculator.setResult(value);
+    }
+
+    @Then("the result should be {double}")
+    public void theResultShouldBe(double result) {
+        Assert.assertEquals(this.calculator.getResult(), result, 0);
+    }
 
 }
